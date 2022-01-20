323a38220677a07c8c7787604216fb37d3cdcb87
m kim <84503428+minkimhere@users.noreply.github.com>
2022-01-20 03:44:32 +0000

    28 harshad num added

---
+++ 28_harshad_num.js
@@ -0,0 +1,22 @@
+//  양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+
+
+let x = 10;
+// let x = 12;
+// let x = 11;
+// let x = 13;
+
+solution(x);
+
+function solution(x) {
+  let arr = (x + '').split('');
+  arr = arr.map(cur => parseInt(cur));
+
+  let sum = 0;
+
+  for (let index in arr){
+    sum += arr[index];
+  }
+
+  return !(x%sum) ? true : false
+}
